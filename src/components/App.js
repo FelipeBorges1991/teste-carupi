756d2287452cbe021b28b614f3e86d909ddf281d
Felipe Borges <fcborges_@hotmail.com>
2021-07-20 18:56:15 -0300

    Inicio do projeto / Organização dos componentes iniciais

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
